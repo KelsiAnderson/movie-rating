d79604833ee25100faa10c86215878d4174ce2b7
vagrant <vagrant@vagrant.vm>
2020-10-30 14:28:04 +0000

    updated model.py and crud.py

--- model.py
+++ model.py
@@ -51,6 +51,10 @@ class Rating(db.Model):
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.movie_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
 
+
+    movie = db.relationship('Movie', backref='ratings')
+    user = db.relationship('User', backref='ratings')
+
     def __repr__(self):
         return f'<Rating rating_id={self.rating_id} score={self.score}>'
 
